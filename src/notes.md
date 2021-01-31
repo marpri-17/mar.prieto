fe979f2ebea5c2b41ce207c7d3497158b5085eba
Mar Prieto N <mar.pnieto@gmail.com>
2021-01-31 18:14:11 +0100

    WIP

---
+++ src/notes.md
@@ -0,0 +1,18 @@
+use swipper for parallax
+API: https://swiperjs.com/swiper-api
+EXAMPLE: https://codepen.io/md-aqil/pen/QeMrRW
+
+
+**PRIORIDADES**
+ 1. Diseño básico y definición de la información
+ 3. Responsive desing and testing
+ 2. Versión en inglés
+**Sections**
+- (navigation bar dynamic and in laterall)
+- HOME
+- ABOUT ME
+- JOBS
+- CONTACT (SOCIAL)
+- FOOTER - redes sociales
+**Design**
+DESKTOP VERSION: Fondo fijo, con scrolling vertical, texto tipo parallax 
---
+++ src/swipper/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+console.log("Ready");
+import Swiper from 'swiper';
+
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
